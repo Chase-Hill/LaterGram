b46c70fe96dcda324f8652685b2db5077c001d88
Chase <chaseaohill@gmail.com>
2023-03-14 17:08:25 -0400

    Added convenience init to model.

--- LaterGram Assessment 7?/LaterGram Assessment 7?/Models/Post.swift
+++ LaterGram Assessment 7?/LaterGram Assessment 7?/Models/Post.swift
@@ -9,7 +9,15 @@ import UIKit
 
 class Post {
     
-    let postTitle: String
+    enum Key {
+        static let user = "user"
+        static let body = "body"
+        static let date = "date"
+        static let image = "image"
+        static let uuid = "uuid"
+    }
+    
+    let postUser: String
     let postBody: String
     let postDate: Date
     let postImage: UIImage?
@@ -17,20 +25,32 @@ class Post {
     
     // No taxation without
     var dictionaryRepresentation: [String : AnyHashable] {
-        ["title": self.postTitle,
-         "post": self.postBody,
-         "date": self.postDate,
-         "image": self.postImage,
-         "uuid": self.uuid
+        [Key.user: self.postUser,
+         Key.body: self.postBody,
+         Key.date: self.postDate,
+         Key.image: self.postImage,
+         Key.uuid: self.uuid
         ]
     }
     
-    init(postTitle: String, postBody: String, postDate: Date = Date(), postImage: UIImage?, uuid: String = UUID().uuidString) {
-        self.postTitle = postTitle
+    init(postUser: String, postBody: String, postDate: Date = Date(), postImage: UIImage?, uuid: String = UUID().uuidString) {
+        self.postUser = postUser
         self.postBody = postBody
         self.postDate = postDate
         self.postImage = postImage
         self.uuid = uuid
+    }
+}
+
+extension Post {
+    convenience init?(fromDictionary dictionary: [String: Any]) {
+        guard let user = dictionary[Key.user] as? String,
+              let body = dictionary[Key.body] as? String,
+              let date = dictionary[Key.date] as? Date,     // Check here
+              let image = dictionary[Key.image] as? UIImage,     // Check here
+              let uuid = dictionary[Key.uuid] as? String else { print("Check Model for bullshittery") ; return nil }
+        
+        self.init(postUser: user, postBody: body, postDate: date, postImage: image, uuid: uuid)
     }
 }
 
